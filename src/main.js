4b9f2ab3f482176bb2d299ff38e44533315e02ca
StepaAv <353833@gmail.com>
2019-08-06 23:24:34 +0300

    3 cards added

--- src/main.js
+++ src/main.js
@@ -1,9 +1,10 @@
- 'use strict'
+'use strict'
 
-const mainControlBlock = document.querySelector('.main__control');
+ const mainControlBlock = document.querySelector('.main__control');
+ const mainMainBlock = document.querySelector('.main');
 
-const getBlockMenu = function () {
-  return `
+ const createBlockMenu = function() {
+   return `
   <section class="control__btn-wrap">
           <input
             type="radio"
@@ -32,8 +33,194 @@ const getBlockMenu = function () {
             >STATISTICS</label
           >
         </section>`;
-};
+ };
 
-mainControlBlock.insertAdjacentHTML(`beforeend`, getBlockMenu());
-mainControlBlock.insertAdjacentHTML(`beforeend`, "blbalba");
+ const createBlockSearch = function() {
+   return `
+  <section class="main__search search container">
+        <input
+          type="text"
+          id="search__input"
+          class="search__input"
+          placeholder="START TYPING — SEARCH BY WORD, #HASHTAG OR DATE"
+        />
+        <label class="visually-hidden" for="search__input">Search</label>
+      </section>`;
+ };
+
+ const createBlockBoard = function () {
+ return `
+       <section class="board container">
+       ${createBlockFilters()}
+       ${createBlockCard(3)}
+       ${createBlockLoadMore()}
+       </section>
+`;
+ };
+
+ const createBlockFilters = function() {
+   return `
+  <section class="main__filter filter container">
+        <input
+          type="radio"
+          id="filter__all"
+          class="filter__input visually-hidden"
+          name="filter"
+          checked
+        />
+        <label for="filter__all" class="filter__label">
+          All <span class="filter__all-count">13</span></label
+        >
+        <input
+          type="radio"
+          id="filter__overdue"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+        />
+        <label for="filter__overdue" class="filter__label"
+          >Overdue <span class="filter__overdue-count">0</span></label
+        >
+        <input
+          type="radio"
+          id="filter__today"
+          class="filter__input visually-hidden"
+          name="filter"
+          disabled
+        />
+        <label for="filter__today" class="filter__label"
+          >Today <span class="filter__today-count">0</span></label
+        >
+        <input
+          type="radio"
+          id="filter__favorites"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__favorites" class="filter__label"
+          >Favorites <span class="filter__favorites-count">1</span></label
+        >
+        <input
+          type="radio"
+          id="filter__repeating"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__repeating" class="filter__label"
+          >Repeating <span class="filter__repeating-count">1</span></label
+        >
+        <input
+          type="radio"
+          id="filter__tags"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__tags" class="filter__label"
+          >Tags <span class="filter__tags-count">1</span></label
+        >
+        <input
+          type="radio"
+          id="filter__archive"
+          class="filter__input visually-hidden"
+          name="filter"
+        />
+        <label for="filter__archive" class="filter__label"
+          >Archive <span class="filter__archive-count">115</span></label
+        >
+      </section>
+            <section class="board container">
+        <div class="board__filter-list">
+          <a href="#" class="board__filter">SORT BY DEFAULT</a>
+          <a href="#" class="board__filter">SORT BY DATE up</a>
+          <a href="#" class="board__filter">SORT BY DATE down</a>
+        </div>`;
+ };
+
+ const createBlockCard = function(repeat = 1) {
+   let cardsAmount = '';
+   for (let i = 0; i < repeat; i++) {
+     cardsAmount += `
+<article class="card card--black">
+            <div class="card__form">
+              <div class="card__inner">
+                <div class="card__control">
+                ${createBlockCardControl()}
+              </div>
+                <div class="card__color-bar">
+                  <svg class="card__color-bar-wave" width="100%" height="10">
+                    <use xlink:href="#wave"></use>
+                  </svg>
+                </div>
+                <div class="card__textarea-wrap">
+                  <p class="card__text">Example default task with default color.</p>
+                </div>
+                <div class="card__settings">
+                  <div class="card__details">
+                    <div class="card__dates">
+                      <div class="card__date-deadline">
+                        <p class="card__input-deadline-wrap">
+                          <span class="card__date">23 September</span>
+                          <span class="card__time">11:15 PM</span>
+                        </p>
+                      </div>
+                    </div>
+
+                    <div class="card__hashtag">
+                      <div class="card__hashtag-list">
+                        <span class="card__hashtag-inner">
+                          <span class="card__hashtag-name">
+                            #todo
+                          </span>
+                        </span>
+
+                        <span class="card__hashtag-inner">
+                          <span class="card__hashtag-name">
+                            #personal
+                          </span>
+                        </span>
+
+                        <span class="card__hashtag-inner">
+                          <span class="card__hashtag-name">
+                            #important
+                          </span>
+                        </span>
+                      </div>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
+  `;
+   };
+   return cardsAmount;
+ };
+
+ const createBlockCardControl = function() {
+   return `
+                  <button type="button" class="card__btn card__btn--edit">
+                    edit
+                  </button>
+                  <button type="button" class="card__btn card__btn--archive">
+                    archive
+                  </button>
+                  <button
+                    type="button"
+                    class="card__btn card__btn--favorites card__btn--disabled"
+                  >
+                    favorites
+                  </button>
+`;
+ };
+
+ const createBlockLoadMore = function () {
+  return `
+    <button class="load-more" type="button">load more</button>
+  `;
+ };
+
+
+ mainControlBlock.insertAdjacentHTML(`beforeend`, createBlockMenu());
+ mainMainBlock.insertAdjacentHTML(`beforeend`, createBlockSearch());
+ mainMainBlock.insertAdjacentHTML(`beforeend`, createBlockBoard());
 
