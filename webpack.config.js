0f7cdb29d24c82073d8cc11f1b04fb359097c9b5
StepaAv <353833@gmail.com>
2019-08-09 21:19:36 +0300

    webpack localhost fix

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require(`path`);
+
 module.exports = {
  mode: `development`, // Режим сборки
  entry: `./src/main.js`, // Точка входа приложения
@@ -9,7 +10,7 @@ module.exports = {
  devtool: 'source-map', // podkliucajem sourcemap
  devServer: {
  contentBase: path.join(__dirname, `public`), // Где искать сборку
- publicPath: 'http:!/localhost:8080/webpack-dev-server/', // Веб адрес сборки
+ publicPath: 'http://localhost:8080', // Веб адрес сборки
  compress: true, // Сжатие
  // Автоматическая перезагрузка страницы
  // Если не работает по стандартному URLу в браузере ‘http:!/localhost:8080’,
